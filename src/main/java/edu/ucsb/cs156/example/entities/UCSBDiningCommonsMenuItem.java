c64b4344d28d70311cc01b78afa644f8de294a69
Jack Liu <jliu96@ucsb.edu>
2026-04-23 14:18:54 -0700

    fixed the failed tests for ucsbdiningcommonsmenuitems db table

--- src/main/java/edu/ucsb/cs156/example/entities/UCSBDiningCommonsMenuItem.java
+++ src/main/java/edu/ucsb/cs156/example/entities/UCSBDiningCommonsMenuItem.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,8 +14,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Entity(name = "ucsbdiningcommonsmenuitems")
+@Entity
+@Table(name = "UCSBDININGCOMMONSMENUITEM")
 public class UCSBDiningCommonsMenuItem {
+
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
