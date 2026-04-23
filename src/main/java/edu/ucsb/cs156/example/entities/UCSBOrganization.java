ba35dbe93bfecf7ced44c3c25ad1a6da7afe39b2
Nikita Huynh <nikitahuynh@ucsb.edu>
2026-04-22 21:27:03 -0700

    partly finished database task

---
+++ src/main/java/edu/ucsb/cs156/example/entities/UCSBOrganization.java
@@ -0,0 +1,26 @@
+package edu.ucsb.cs156.example.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * This is a JPA entity that represents a UCSBDiningCommons
+ *
+ * <p>A UCSBDiningCommons is a dining commons at UCSB
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "ucsborganizations")
+public class UCSBOrganization {
+  @Id private String code;
+  private String orgCode;
+  private String orgTranslationShort;
+  private String orgTranslation;
+  private boolean inactive;
+}
