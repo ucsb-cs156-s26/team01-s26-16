765b46ea87dd7bfebb05741124d02ee7574171b0
Ivy Holiday <iholiday@ucsb.edu>
2026-04-27 17:16:53 -0700

    Implemented DELETE method in RecommendationRequestController.java

--- src/main/java/edu/ucsb/cs156/example/controllers/RecommendationRequestController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/RecommendationRequestController.java
@@ -13,6 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -139,5 +140,24 @@ public class RecommendationRequestController extends ApiController {
     recommendationRequestRepository.save(recommendationRequest);
 
     return recommendationRequest;
+  }
+
+  /**
+   * Delete a RecommendationRequest
+   *
+   * @param id the id of the recommendation request to delete
+   * @return a message indicating the recommendation request was deleted
+   */
+  @Operation(summary = "Delete a RecommendationRequest")
+  @PreAuthorize("hasRole('ROLE_ADMIN')")
+  @DeleteMapping("")
+  public Object deleteRecommendationRequest(@Parameter(name = "id") @RequestParam Long id) {
+    RecommendationRequest recommendationRequest =
+        recommendationRequestRepository
+            .findById(id)
+            .orElseThrow(() -> new EntityNotFoundException(RecommendationRequest.class, id));
+
+    recommendationRequestRepository.delete(recommendationRequest);
+    return genericMessage("RecommendationRequest with id %s deleted".formatted(id));
   }
 }
